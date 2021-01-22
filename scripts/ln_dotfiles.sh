2a98944acab2837e7001cbc3b7e5bd1141b917f3
ShoheiKamiya <shoooooo.bqt@gmail.com>
2021-01-22 22:43:54 +0900

    add nvim configration

--- scripts/ln_dotfiles.sh
+++ scripts/ln_dotfiles.sh
@@ -1,2 +1,3 @@
 # ~/.xxx が ~/my-setup/dotfiles/.xxx を参照するようにシンボリックリンクを貼る
 ls -a1 ~/my-setup/dotfiles | grep -e "\.[a-z]" | xargs -I@ ln -sf ~/my-setup/dotfiles/@ ~/@
+ln -fs ~/my-setup/dotfiles/init.vim ~/.config/nvim/init.vim

968843dc53f0a4b3262af3e1983ffe0117b99534
ShoheiKamiya <shoooooo.bqt@gmail.com>
2021-01-24 08:06:24 +0900

    Create intermediate directories as required

--- scripts/ln_dotfiles.sh
+++ scripts/ln_dotfiles.sh
@@ -1,4 +1,4 @@
 # ~/.xxx が ~/my-setup/dotfiles/.xxx を参照するようにシンボリックリンクを貼る
 ls -a1 ~/my-setup/dotfiles | grep -e "\.[a-z]" | xargs -I@ ln -sf ~/my-setup/dotfiles/@ ~/@
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 ln -fs ~/my-setup/dotfiles/init.vim ~/.config/nvim/init.vim
